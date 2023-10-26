9859b14496e7d2042567939301ca4607d84d65df
Lucanb <lucanastasa@yahoo.com>
2023-10-26 10:06:32 +0300

    almost

--- Homework1/Lab2.py
+++ Homework1/Lab2.py
@@ -1,128 +1,79 @@
-import numpy as np
-def Initialisation_State() :
-    matrix = np.array([[2, 7, 5], [0, 8, 4], [3, 1, 6]])
-    value = [1, 0]
-    return matrix, value
+from collections import deque
 
-def Final_State_Verif(matrix):
-    values_matrix=[]
-    for i in range(3):
+class SolvePuzzle:
+    def __init__(self):
+        self.initial_state = None
+    def Verif(self,state):
+      values_matrix=[]
+      for i in range(3):
        for j in range(3):
-            if(matrix[i][j] != 0):  
-               values_matrix.append(matrix[i][j])
+            if(state[i][j] != 0):  
+               values_matrix.append(state[i][j])
 
-    for i in range(1, len(values_matrix)):
+      for i in range(1, len(values_matrix)):
        if values_matrix[i] < values_matrix[i - 1]:
             return False
-    return True
-
-def isInMAtrix(i,j):
-   if i>=0 and i<3 and j>=0 and j<3 :
-    return True
-   else :
-    return False
-   
-def Transition(matrix, values, next):
-   if isInMAtrix(next[0], next[1]):
-      const = matrix[next[0]][next[1]]
-      LastNumber = matrix[next[0]][next[1]] 
-      matrix[next[0]][next[1]] = matrix[values[0]][values[1]]
-      matrix[values[0]][values[1]] = const
-      return matrix, next, LastNumber
-   else:
-      return None, None,None
-   
-def Memorise_State(matrix,unique_matrices):
-
-    if matrix.tobytes() in unique_matrices:
-      return False
-    else:
       return True 
+    
+    def posNeigbourhood(self,state):
+       neighbours = []
+       row,col = None,None
 
-def LastPosition(last_number,matrix,next):
-   if matrix[next[0]][next[1]] == last_number:
-      return False
-   return True   
+       for i in range(3):
+          for j in range(3):
+           if state[i][j] == 0:
+              row,col=i,j
+       vec = [(0,1),(0,-1),(1,0),(-1,0)]
+       for l,p in vec:
+                 newRow = row + l
+                 newCol = col + p
+                 if 0<=newRow and newRow < 3 and newCol>=0  and newCol < 3:
+                    newState = [row[:] for row in state]
+                    newState[row][col],newState[newRow][newCol]=newState[newRow][newCol],newState[row][col]
+                    neighbours.append(newState)
 
-def get_distances(matrix,values):
-   n_up = values[0] + 1  # urc pe linie
-   next_1 = [n_up,values[1]]
-   n_down = values[0] - 1 #cobor pe linie
-   next_2 = [n_down,values[1]]
-   n_left = values [1] - 1  #stanga
-   next_3 = [values[0],n_left]
-   n_right = values[1] +1  #dreapta
-   next_4 = [values[0],n_right]
-   # daca pozitia nu este valida voi da o distanta foarte mare care sa nu fie luata in calcul
-   if(isInMAtrix(n_up,values[1])):
-    distance1 = ((values[0]-n_up)**2 + values[1]**2)**1/2 
-   else:
-    distance1 = 1000000
-   if(isInMAtrix(n_down,values[1])):
-    distance2 = ((values[0]-n_down)**2 + values[1]**2)**1/2
-   else:
-    distance2 = 1000000
-   if(isInMAtrix(values[0],n_left)):
-    distance3 = (values[0]**2 + (n_left-values[1])**2)**1/2
-   else:
-    distance3 = 1000000
-   if(isInMAtrix(values[0],n_right)):
-    distance4 = (values[0]**2 + (n_right-values[1])**2)**1/2    
-   else:
-    distance4 = 1000000  
-   #print([distance1,distance2, distance3,distance4],[next_1,next_2,next_3,next_4])
-   return  [distance1,distance2, distance3,distance4],[next_1,next_2,next_3,next_4]
+       return neighbours
 
-def IDDFS(unique_matrices, matrix, values, depth, maxDepth):
-   LastNumber = 0
-   if depth > maxDepth:
-      return None
-   if Final_State_Verif(matrix):
-      return matrix
-   distances, positions = get_distances(matrix, values)
-   for position in positions:
-      if (isInMAtrix(position[0], position[1]) and
-          Memorise_State(matrix, unique_matrices) and
-          LastPosition(LastNumber, matrix, position)):
-         unique_matrices.add(matrix.tobytes())
-         matrix, values, LastNumber = Transition(matrix, values, position)
-         if matrix is not None and values is not None and LastNumber is not None:
-            print(matrix)
-            result = IDDFS(unique_matrices, matrix, values, depth + 1, maxDepth)
-            if result is not None:
-               return result
-   return None
+    def IDDFS(self,initialState):
+      self.initial_state = initialState
+      DepthMax = 0
+      while True:
+          visited = set()     
+          stack = deque([(self.initial_state,0)])
 
-def DoIDDFS(matrix, values):
-   unique_matrices = set()
-   maxDepth = 0
-   depth = 0
-   while True:
-      result = IDDFS(unique_matrices, matrix, values, depth, maxDepth)
-      if result is not None:
-         return result
-      maxDepth += 1
+          while stack:
+             state,depth = stack.pop()
+             visited.add(str(state))
+             print(state)
+             if self.Verif(state):
+                  return state
+             if depth < DepthMax:
+                for position in self.posNeigbourhood(state):
+                   if str(position) not in visited:
+                      stack.append((position,depth + 1)) 
+          DepthMax += 1
+
+    def printState(self,state):
+       for row in state:
+            print(' '.join(map(str,row)))
 
 if __name__ == '__main__':
-    matrix,values = Initialisation_State() 
+    initial_state = [
+        [2, 7, 5], 
+        [0, 8, 4], 
+        [3, 1, 6]
+    ]
 
-    solution = DoIDDFS(matrix,values)
-    # Afișarea soluției
-    if solution:
-        print('Puzzle solved')
+    puzzle = SolvePuzzle()
+    result = puzzle.IDDFS(initial_state)
+
+    if result:
+        print("Soluție găsită:")
+        puzzle.print_state(result)
     else:
-        print("Puzzle cannot be solved.")            
-
-####DEBUG
-    # matrix = np.array([[1, 2, 0], [3, 4, 5], [6, 7, 8]])
-    # values = [0,2]
-    # next = [0,1]
-    # matrix, values, LastNumber = Transition(matrix,values,next)
-    # print(matrix,values,LastNumber)
+        print("Nu există soluție.")        
 
 
 
 
-
-
-
+                    
--- Homework1/Lab3.py
+++ Homework1/Lab3.py
@@ -1,138 +1,145 @@
+from collections import deque
+import time
+from queue import PriorityQueue
 import numpy as np
-def Initialisation_State() :
-    matrix = np.array([[2, 7, 5], [0, 8, 4], [3, 1, 6]])
-    value = [1, 0]
-    return matrix, value
 
-def Final_State_Verif(matrix):
-    values_matrix=[]
-    for i in range(3):
+class SolvePuzzle:
+    def __init__(self,initial_state):
+        self.initial_state = None
+    def Verif(self,state):
+      values_matrix=[]
+      for i in range(3):
        for j in range(3):
-            if(matrix[i][j] != 0):  
-               values_matrix.append(matrix[i][j])
+            if(state[i][j] != 0):  
+               values_matrix.append(state[i][j])
 
-    for i in range(1, len(values_matrix)):
+      for i in range(1, len(values_matrix)):
        if values_matrix[i] < values_matrix[i - 1]:
             return False
-    return True
-
-def isInMAtrix(i,j):
-   if i>=0 and i<3 and j>=0 and j<3 :
-    return True
-   else :
-    return False
-   
-def Transition(matrix, values, next):
-   if isInMAtrix(next[0], next[1]):
-      const = matrix[next[0]][next[1]]
-      LastNumber = matrix[next[0]][next[1]] 
-      matrix[next[0]][next[1]] = matrix[values[0]][values[1]]
-      matrix[values[0]][values[1]] = const
-      return matrix, next, LastNumber
-   else:
-      return None, None
-   
-def Memorise_State(matrix,unique_matrices):
-
-    if matrix.tobytes() in unique_matrices:
-      return False
-    else:
       return True 
+    
+    @staticmethod
+    def posNeigbourhood(state):
+       neighbours = []
+       row,col = None,None
 
-def LastPosition(last_number,matrix,next):
-   if matrix[next[0]][next[1]] == last_number:
-      return False
-   return True   
+       for i in range(3):
+          for j in range(3):
+           if state[i][j] == 0:
+              row,col=i,j
+       vec = [(0,1),(0,-1),(1,0),(-1,0)]
+       for l,p in vec:
+                 newRow = row + l
+                 newCol = col + p
+                 if 0<=newRow and newRow < 3 and newCol>=0  and newCol < 3:
+                    newState = [row[:] for row in state]
+                    newState[row][col],newState[newRow][newCol]=newState[newRow][newCol],newState[row][col]
+                    neighbours.append(newState)
 
-def get_distances(matrix,values):
-   n_up = values[0] + 1  # urc pe linie
-   next_1 = [n_up,values[1]]
-   n_down = values[0] - 1 #cobor pe linie
-   next_2 = [n_down,values[1]]
-   n_left = values [1] - 1  #stanga
-   next_3 = [values[0],n_left]
-   n_right = values [1] +1  #dreapta
-   next_4 = [values[0],n_right]
-   # daca pozitia nu este valida voi da o distanta foarte mare care sa nu fie luata in calcul
-   if(isInMAtrix(n_up,values[1])):
-    distance1 = ((values[0]-n_up)**2 + values[1]**2)**1/2 
-   else:
-    distance1 = 1000000
-   if(isInMAtrix(n_down,values[1])):
-    distance2 = ((values[0]-n_down)**2 + values[1]**2)**1/2
-   else:
-    distance2 = 1000000
-   if(isInMAtrix(values[0],n_left)):
-    distance3 = (values[0]**2 + (n_left-values[1])**2)**1/2
-   else:
-    distance3 = 1000000
-   if(isInMAtrix(values[0],n_right)):
-    distance4 = (values[0]**2 + (n_right-values[1])**2)**1/2    
-   else:
-    distance4 = 1000000  
-   #print([distance1,distance2, distance3,distance4],[next_1,next_2,next_3,next_4])
-   return  [distance1,distance2, distance3,distance4],[next_1,next_2,next_3,next_4]
+       return neighbours
 
-def IDDFS(unique_matrices, matrix, values, depth, maxDepth):
-   LastNumber = 0
-   if depth > maxDepth:
-      return None
-   if Final_State_Verif(matrix):
-      return matrix
-   distances, positions = get_distances(matrix, values)
-   for position in positions:
-      if (isInMAtrix(position[0], position[1]) and
-          Memorise_State(matrix, unique_matrices) and
-          LastPosition(LastNumber, matrix, position)):
-         unique_matrices.add(matrix.tobytes())
-         matrix, values, LastNumber = Transition(matrix, values, position)
-         if matrix is not None and values is not None and LastNumber is not None:
-            print(matrix)
-            result = IDDFS(unique_matrices, matrix, values, depth + 1, maxDepth)
-            if result is not None:
-               return result
-   return None
-
-def DoIDDFS(matrix, values):
-   unique_matrices = set()
-   maxDepth = 0
-   depth = 0
-   while True:
-      result = IDDFS(unique_matrices, matrix, values, depth, maxDepth)
-      if result is not None:
-         return result
-      maxDepth += 1
+def manhattanDistance(state):
+       normalAranj = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+       distance = 0
+       for i in range(3):
+          for j in range(3):
+             if state[i][j] != 0:
+                row,col = np.where(normalAranj == state[i][j]) 
+                distance += abs(i-row[0]) + abs(j-col[0])
+       return distance
+            
+def hammingDistance(state):
+        normalAranj = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+        return np.sum(state != normalAranj) - 1 
 
 
-def GreedyAlg():
-   
-   return
+def euclidDistance(state):    
+       normalAranj = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+       distance = 0
+       for i in range(3):
+         for j in range(3):
+            if state[i][j] != 0:
+                row, col = np.where(normalAranj == state[i][j])
+                distance += np.sqrt((i - row[0]) ** 2 + (j - col[0]) ** 2)
+       return distance   
 
+def Greedy(puzzle,heuristic):
+        pq = PriorityQueue()
+        initial_state_tuple = tuple(map(tuple, puzzle.initial_state))
+        pq.put((heuristic(puzzle.initial_state), initial_state_tuple))
+        visited = set()
+        parent = {}
+        while not pq.empty():
+            _, state = pq.get()
+            state_array = np.array(state)
+            if puzzle.is_final(state_array):
+              solution = [state_array]
+              while not np.array_equal(state_array, puzzle.initial_state):
+                state_array = parent[state_array]
+                solution.insert(0, state_array)
+              return solution
+            visited.add(state)
+            for neighbor in puzzle.get_neighbors(state_array):
+                if neighbor is not None:
+                    tuple_state = tuple(map(tuple, neighbor))
+                    if tuple_state not in visited:
+                     pq.put((heuristic(neighbor), tuple_state))
+                     parent[tuple_state] = state_array
+        return None
 
+def IDDFS(puzzle, max_depth):
+        def dls(node, depth):
+            if depth == 0:
+                return [node]
+            for neighbor in puzzle.get_neighbors(node):
+                if neighbor is not None:
+                    path = dls(neighbor, depth - 1)
+                    if path is not None:
+                        return [node] + path
+            return None
 
+        for depth in range(max_depth + 1):
+            solution = dls(puzzle.initial_state, depth)
+            if solution is not None:
+                return solution
+        return None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def printState(self,state):
+        for row in state:
+                print(' '.join(map(str,row)))
 
 if __name__ == '__main__':
-    matrix,values = Initialisation_State() 
+    # Inițializați instanțele de puzzle și euristicile
+    instances = [np.array([[8, 6, 7], [2, 5, 4], [0, 3, 1]]),
+                 np.array([[2, 5, 3], [1, 0, 6], [4, 7, 8]]),
+                 np.array([[8, 7, 1], [6, 0, 2], [5, 4, 3]])]
 
-    solution = DoIDDFS(matrix,values)
-    # Afișarea soluției
-    if solution:
-        print('Puzzle solved')
-    else:
-        print("Puzzle cannot be solved.")            
+    heuristics = [manhattanDistance, hammingDistance, euclidDistance]
+
+    for puzzle_instance in instances:
+        puzzle = SolvePuzzle(puzzle_instance)
+        for heuristic in heuristics:
+            start_time = time.time()
+            greedy_solution = Greedy(puzzle, heuristic)
+            end_time = time.time()
+            if greedy_solution:
+                print(f'Greedy {heuristic.__name__} heuristic:')
+                for state in greedy_solution:
+                    print(state)
+                print(f'Solution length: {len(greedy_solution) - 1}')
+                print(f'Time: {end_time - start_time:.4f} seconds')
+            # else:
+            #     print(f'No solution {heuristic.__name__} heuristic.')
+
+            start_time = time.time()
+            iddfs_solution = IDDFS(puzzle, 31)  # 31 is the maximum depth for 8-puzzle
+            end_time = time.time()
+            if iddfs_solution:
+                print('IDDFS solution:')
+                for state in iddfs_solution:
+                    print(state)
+                print(f'Solution length: {len(iddfs_solution) - 1}')
+                print(f'Time : {end_time - start_time:.4f} seconds')
+            else:
+                print('No solution found using IDDFS.')
+            print('-' * 30)        
