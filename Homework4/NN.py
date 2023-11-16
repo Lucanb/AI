7f6e3ea56905eaced9550b2d31a92b0ec215b74d
Lucanb <lucanastasa@yahoo.com>
2023-11-16 10:23:19 +0200

    Done

--- Homework4/NN.py
+++ Homework4/NN.py
@@ -3,38 +3,37 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 class NeuralNetwork:
-    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.01):
-        self.input_size = input_size
+    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.001):
+        self.input = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.learning_rate = learning_rate
 
-        # Initialize weights and biases
         self.W1 = np.random.randn(hidden_size, input_size)
         self.b1 = np.zeros((hidden_size, 1))
         self.W2 = np.random.randn(output_size, hidden_size)
         self.b2 = np.zeros((output_size, 1))
 
-    def sigmoid(self, Z):
-        return 1 / (1 + np.exp(-Z))
+    def sigmoid(self, element):
+        return 1 / (1 + np.exp(-element))
 
     def forward_propagation(self, X):
-        Z1 = np.dot(self.W1, X) + self.b1
-        A1 = np.tanh(Z1)
-        Z2 = np.dot(self.W2, A1) + self.b2
-        A2 = self.sigmoid(Z2)
+        dot_prod = np.dot(self.W1, X) + self.b1
+        activ1 = np.tanh(dot_prod)
+        dot_prod2 = np.dot(self.W2, activ1) + self.b2
+        activ2 = self.sigmoid(dot_prod2)
 
-        cache = {"Z1": Z1, "A1": A1, "Z2": Z2, "A2": A2}
-        return A2, cache
+        cache = {"dot_prod": dot_prod, "activ1": activ1, "dot_prod2": dot_prod2, "activ2": activ2}
+        return activ2, cache
 
     def backward_propagation(self, X, Y, cache):
         m = X.shape[1]
 
-        dZ2 = cache["A2"] - Y
-        dW2 = 1 / m * np.dot(dZ2, cache["A1"].T)
+        dZ2 = cache["activ2"] - Y
+        dW2 = 1 / m * np.dot(dZ2, cache["activ1"].T)
         db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
 
-        dZ1 = np.dot(self.W2.T, dZ2) * (1 - np.power(cache["A1"], 2))
+        dZ1 = np.dot(self.W2.T, dZ2) * (1 - np.power(cache["activ1"], 2))
         dW1 = 1 / m * np.dot(dZ1, X.T)
         db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
 
@@ -48,37 +47,26 @@ class NeuralNetwork:
 
     def train(self, X, Y, epochs=1000):
         for epoch in range(epochs):
-            # Forward propagation
-            A2, cache = self.forward_propagation(X)
-
-            # Compute cost
-            cost = -1 / X.shape[1] * np.sum(Y * np.log(A2) + (1 - Y) * np.log(1 - A2))
-
-            # Backward propagation
+            activ2, cache = self.forward_propagation(X)
+            cost = -1 / X.shape[1] * np.sum(Y * np.log(activ2) + (1 - Y) * np.log(1 - activ2))
             grads = self.backward_propagation(X, Y, cache)
-
-            # Update parameters
             self.update_parameters(grads)
-
-            # Print cost every 100 epochs
             if epoch % 100 == 0:
                 print(f"Epoch {epoch}, Cost: {cost}")
 
     def predict(self, X):
-        A2, _ = self.forward_propagation(X)
-        predictions = (A2 > 0.5).astype(int)
+        activ2, _ = self.forward_propagation(X)
+        predictions = (activ2 > 0.5).astype(int)
         return predictions
 
 def read_data(file_url, column_names):
     df = pd.read_csv(file_url, sep="\s+", header=None, names=column_names)
     return df
 
-# Read data
 data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00236/seeds_dataset.txt"
-column_names = ["area", "perimeter", "compactness", "length_of_kernel", "width_of_kernel", "asymmetry_coefficient", "length_of_kernel_groove", "class"]
+column_names = ["atribute1", "atribute2", "atribute3", "atribute4", "atribute5", "atribute6", "atribute7", "class"]
 df = read_data(data_url, column_names)
 
-# Split data into training and testing sets
 train_data, test_data, train_labels, test_labels = train_test_split(
     df.drop("class", axis=1),
     df["class"],
@@ -86,24 +74,19 @@ train_data, test_data, train_labels, test_labels = train_test_split(
     random_state=42
 )
 
-# Transpose the data for neural network training
 train_data = train_data.T.values
 test_data = test_data.T.values
 
-# One-hot encode the labels
 train_labels = (train_labels - 1).values.reshape(1, -1)
 test_labels = (test_labels - 1).values.reshape(1, -1)
 
-# Create and train the neural network
 input_size = train_data.shape[0]
 hidden_size = 4
 output_size = 1
-neural_net = NeuralNetwork(input_size, hidden_size, output_size, learning_rate=0.01)
+neural_net = NeuralNetwork(input_size, hidden_size, output_size, learning_rate=0.001)
 neural_net.train(train_data, train_labels, epochs=1000)
 
-# Make predictions on the test set
 predictions = neural_net.predict(test_data)
 
-# Print accuracy
 accuracy = np.mean(predictions == test_labels)
 print(f"Accuracy: {accuracy}")
